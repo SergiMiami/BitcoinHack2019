635b22095254cbd6bc9894be71940d01dabbc6d1
notmike-5 <notmike55@gmail.com>
2019-01-19 12:16:47 -0500

    message

---
+++ Space.py
@@ -0,0 +1,35 @@
+
+
+class Space:
+
+    def __init__(self, attributes):
+        self.name = attributes['name']
+        self.pos = attributes['position']
+
+
+class Property(Space):
+
+    def __init__(self, attributes):
+
+        Space.__init__(self, attributes)
+
+        self.price = attributes['price']
+        self.rent = self.price/2
+        self.mortgage = False
+        self.owner = None
+
+class Street(Property):
+    pass
+
+class Railroad(Property):
+    pass
+
+class Utility(Property):
+    pass
+
+class Tax(Space):
+    """Tax Space"""
+    #Taxes are static is a space really best?
+    def __init__(selfself, attributes):
+        Space.__init__(self, attributes)
+        self.tax = attributes['tax']
