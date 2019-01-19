8684b08513612d932af216ac4bd4d2499850552f
notmike-5 <notmike55@gmail.com>
2019-01-19 18:38:09 -0500

    Bank genAdddress()

--- Bank.py
+++ Bank.py
@@ -1,6 +1,19 @@
+from uuid import uuid4
+from block_io import BlockIo
+from random import randint
 
 class Bank:
 
+    #API Gateway
+    io = BlockIo('226a-4ade-8eb4-1fed', '696996969')
+
     def __init__(self):
-        self.id = 0
+        self.id = uuid4()
         self.balance = 0
+
+
+    def genAddress(self):
+        return(Bank.io.get_new_address())
+
+    def genAddress(self, label=randint(0,100)):
+        return(Bank.io.get_new_address(label=label))
