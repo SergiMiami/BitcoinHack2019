f0b8ea44885a0b40c537393226d6c2162fae6a12
Sergi Sparks <sergii.galaganiuk@oracle.com>
2019-01-20 00:51:46 -0500

    requirements file

--- bcode/bcode/views.py
+++ bcode/bcode/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import sys
 
 def welcome(request):
-    return render(request, 'bcode/welcome.html')
+    if request.user.is_authenticated:
+        return redirect('player_home')
+    else:
+        return render(request, 'bcode/welcome.html')
