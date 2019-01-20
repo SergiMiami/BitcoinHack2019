0748d2ad091ad6e32cc08e4ba72588c09ea1dffe
Sergi Sparks <sergii.galaganiuk@oracle.com>
2019-01-19 22:03:31 -0500

    working now

--- bcode/player/views.py
+++ bcode/player/views.py
@@ -4,22 +4,18 @@ from gameplay.models import Game
 
 
 def home(request):
-    games_first_player = Game.objects.filter(
-        first_player = request.user,
+    games_first_player = Game.object.filter(
+        first_player=request.user,
         status='F'
     )
-    games_second_player = Game.objects.filter(
+    games_second_player = Game.object.filter(
         second_player=request.user,
         status='S'
     )
     all_my_games = list(games_first_player) + list(games_second_player)
 
-
-    #    .games_for_user(request.user)
-    #active_games = my_games.active()
-
     return render(request, "player/home.html",
                   {'games': all_my_games,
-                   'num_games': Game.objects.count(),
+                   'num_games': Game.object.count(),
                    'n_games': len(all_my_games)})
 
